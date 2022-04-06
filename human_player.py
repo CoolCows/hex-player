fb2be312483f7e5e043f6f1bb0db2cdddf6f9e0e
rodrigo-pino <rodrodpino@gmail.com>
2022-04-06 07:29:11 -0400

    Delete files

--- human_player.py
+++
@@ -1,9 +0,0 @@
-def play(game, player):
-    print("Human Play")
-    print(game)
-    try:
-        x, y = list(map(int, input().split()))
-    except:
-        print("Invalid input")
-        return play(game, player)
-    return x, y
